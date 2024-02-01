2c959244ae97c5619d08e67b28b6f14c76f0d8aa
Владіслав Лоєнко <74206645+vloienko@users.noreply.github.com>
2024-02-01 23:19:06 +0200

    page main - Done

---
+++ vite-project/src/components/section/BgImage/BgImage.jsx
@@ -0,0 +1,40 @@
+import mainBG from './../../../img/main-bg.jpg';
+
+
+const BgImage = () => {
+   return (
+      <div className="bg-image margin-top">
+         <div className="bg-image__container">
+            <div className="bg-image__body">
+               <div className="bg-image__image">
+                  <img src={mainBG} alt="Image background" />
+               </div>
+               <div className="bg-image__block">
+                  <div className="bg-image__box">
+                     <span className="bg-image__descr">Current Bid:</span>
+                     <div className="bg-image__holder">
+                        <div className="bg-image__content image">
+                           1,5M<span>(1308.54$)</span>
+                        </div>
+                     </div>
+                  </div>
+                  <div className="bg-image__box">
+                     <span className="bg-image__descr">Auction ending in:</span>
+                     <div className="bg-image__holder">
+                        <div className="bg-image__content">
+                           1h 28m 32s
+                        </div>
+                     </div>
+                  </div>
+                  <div className="bg-image__box">
+                     <a className="bg-image__link" href="#">Place a bid</a>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      
+   );
+}
+ 
+export default BgImage;
--- vite-project/src/pages/ArtworkPage.jsx
+++ vite-project/src/pages/ArtworkPage.jsx
@@ -1,6 +1,12 @@
+import BgImage from '../components/section/BgImage/BgImage';
+
+
+
 const ArtworkPage = () => {
    return (
-      <div></div>
+      <>
+         <BgImage />
+      </>
    );
 }
  
